3718aaa5561292b4fbd482385d0261f897bb0ad9
yejin <yaezzin@gmail.com>
2023-01-28 00:49:37 +0900

    feat : Member 엔티티 생성

---
+++ src/main/java/com/app/pingpong/member/entity/Member.java
@@ -0,0 +1,30 @@
+package com.app.pingpong.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String socialId;
+
+    private String email;
+
+    private String nickname;
+
+    private String profileImage;
+
+}
