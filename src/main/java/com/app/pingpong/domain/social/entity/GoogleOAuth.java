9894cdd4e1ea39cd7d29c835b55cf1eebac4ca2c
yejin <yaezzin@gmail.com>
2023-01-28 20:46:36 +0900

    feat : 구글 소셜로그인 정보 조회

--- src/main/java/com/app/pingpong/domain/social/entity/GoogleOAuth.java
+++ src/main/java/com/app/pingpong/domain/social/entity/GoogleOAuth.java
@@ -1,17 +1,99 @@
 package com.app.pingpong.domain.social.entity;
 
 import com.app.pingpong.domain.social.dto.response.UserInfoResponse;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
 public class GoogleOAuth implements SocialOAuth {
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
 
     @Override
     public String getAccessToken(String code) {
-        return "~~~";
+        String accessToken = "";
+        String reqURL = "https://oauth2.googleapis.com/token";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("code=" + code);
+            sb.append("&client_id=" + clientId);
+            sb.append("&client_secret=" + clientSecret);
+            sb.append("&redirect_uri=" + redirectUri);
+            sb.append("&grant_type=authorization_code");
+            bw.write(sb.toString());
+            bw.flush();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            JsonElement element = JsonParser.parseString(result);
+            accessToken = element.getAsJsonObject().get("access_token").getAsString();
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return accessToken;
     }
 
     @Override
     public UserInfoResponse getUserInfo(String accessToken) {
-        return null;
+        HashMap<String, Object> userInfo = new HashMap<>();
+        String reqURL = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            JsonElement element = JsonParser.parseString(result);
+            String email = element.getAsJsonObject().get("email").getAsString();
+            String id = element.getAsJsonObject().get("id").getAsString();
+            userInfo.put("email", email);
+            userInfo.put("id", id);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new UserInfoResponse((String)userInfo.get("id"), (String)userInfo.get("email"));
     }
 
 }
--- src/main/java/com/app/pingpong/domain/social/service/SocialService.java
+++ src/main/java/com/app/pingpong/domain/social/service/SocialService.java
@@ -2,6 +2,7 @@ package com.app.pingpong.domain.social.service;
 
 import com.app.pingpong.domain.social.dto.request.UserInfoRequest;
 import com.app.pingpong.domain.social.dto.response.UserInfoResponse;
+import com.app.pingpong.domain.social.entity.GoogleOAuth;
 import com.app.pingpong.domain.social.entity.KakaoOAuth;
 import com.app.pingpong.global.exception.BaseException;
 import lombok.RequiredArgsConstructor;
@@ -14,19 +15,26 @@ import static com.app.pingpong.global.exception.StatusCode.INVALID_SOCIAL_TYPE;
 public class SocialService {
 
     private final KakaoOAuth kakao;
+    private final GoogleOAuth google;
 
     public UserInfoResponse getUserInfo(UserInfoRequest request) {
-
         switch (request.getSocialType()) {
             case "KAKAO" : {
                 String accessToken = kakao.getAccessToken(request.getCode());
                 UserInfoResponse userInfo = kakao.getUserInfo(accessToken);
                 return userInfo;
             }
-
+            case "GOOGLE" : {
+                String accessToken = google.getAccessToken(request.getCode());
+                UserInfoResponse userInfo = google.getUserInfo(accessToken);
+                return userInfo;
+            }
             default: {
                 throw new BaseException(INVALID_SOCIAL_TYPE);
             }
         }
     }
+
+
+
 }
