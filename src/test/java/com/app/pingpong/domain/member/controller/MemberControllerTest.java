598bdb64b0817649905b55d35493151ff3fc507c
yejin <yaezzin@gmail.com>
2023-05-06 23:22:12 +0900

    test : memberController test 수정

--- src/test/java/com/app/pingpong/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/app/pingpong/domain/member/controller/MemberControllerTest.java
@@ -20,7 +20,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 
-import static org.mockito.ArgumentMatchers.*;
+import static com.app.pingpong.global.common.exception.StatusCode.SUCCESS_SAVE_SEARCH_LOG;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,8 +46,7 @@ public class MemberControllerTest {
     }
 
     @Test
-    @DisplayName("Member Controller - 회원가입")
-    public void signup() throws Exception {
+    public void signupTest() throws Exception {
         // given
         SignUpRequest request = new SignUpRequest("socialId", "email", "nickname", "profile");
 
@@ -54,11 +55,11 @@ public class MemberControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
+        verify(memberService).signup(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 유효성 검증")
-    public void validateNickname() throws Exception {
+    public void validateNicknameTest() throws Exception {
         // given
         String nickname = "member";
 
@@ -66,94 +67,98 @@ public class MemberControllerTest {
         mockMvc.perform(post("/api/members/validate")
                         .param("nickname", nickname))
                 .andExpect(status().isOk());
-        verify(memberService).validateNickname(nickname);
+        verify(memberService).validateNickname(any());
     }
 
     @Test
-    @DisplayName("Member Controller - id로 회원 조회")
-    public void findById() throws Exception {
+    public void findByIdTest() throws Exception {
         // given
         Long id = 1L;
 
         // when, then
         mockMvc.perform(get("/api/members/{id}", id))
                 .andExpect(status().isOk());
-        verify(memberService).findById(id);
+        verify(memberService).findById(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 회원정보 수정")
-    public void update() throws Exception {
+    public void updateTest() throws Exception {
         // given
         Long id = 1L;
-        UpdateRequest update = new UpdateRequest("nickname", "profile");
+        UpdateRequest request = new UpdateRequest("nickname", "profile");
 
         // when, then
         mockMvc.perform(patch("/api/members/{id}", id)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(update)))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
-        verify(memberService).update(anyLong(), any());
+        verify(memberService).update(any(), any());
     }
 
     @Test
-    @DisplayName("Member Controller - 회원탈퇴")
-    public void deleteMember() throws Exception {
+    public void deleteMemberTest() throws Exception {
+        // given
         Long id = 1L;
+
+        // when, then
         mockMvc.perform(delete("/api/members/{id}", id))
                 .andExpect(status().isOk());
-        verify(memberService).delete(id);
+        verify(memberService).delete(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 마이페이지 조회")
-    public void getMyPage() throws Exception {
+    public void getMyPageTest() throws Exception {
+        // given
         Long id = 1L;
+
+        // when, then
         mockMvc.perform(get("/api/members/{id}/mypage", id))
                 .andExpect(status().isOk());
-        verify(memberService).getMyPage(anyLong());
+        verify(memberService).getMyPage(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 상대방 페이지 조회")
-    public void getOppPage() throws Exception {
+    public void getOppPageTest() throws Exception {
+        // given
         Long id = 1L;
+
+        // when, then
         mockMvc.perform(get("/api/members/{id}/profile", id))
                 .andExpect(status().isOk());
-        verify(memberService).getOppPage(anyLong());
+        verify(memberService).getOppPage(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 닉네임로 회원 조회")
-    public void findByNickname() throws Exception {
+    public void findByNicknameTest() throws Exception {
+        // given
         String nickname = "test";
         Long id = 1L;
 
+        // when, then
         mockMvc.perform(get("/api/members/search")
                         .param("nickname", nickname)
                         .param("id", id.toString()))
                 .andExpect(status().isOk());
-        verify(memberService).findByNickname(anyString(), anyLong());
+        verify(memberService).findByNickname(any(), any());
     }
 
     @Test
-    @DisplayName("MemberController - 검색 기록 저장")
-    public void saveSearchLog() throws Exception {
+    public void saveSearchLogTest() throws Exception {
         // given
         Long loginMemberId = 1L;
         SearchLogRequest request = new SearchLogRequest(2L);
 
+        given(memberService.saveSearchLog(any(), any())).willReturn(SUCCESS_SAVE_SEARCH_LOG);
+
         // when, then
         mockMvc.perform(post("/api/members/search-log")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
-                        .header("Authorization", "Bearer access-token"))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
-        verify(memberService).saveSearchLog(eq(request), eq(loginMemberId));
+        verify(memberService).saveSearchLog(any(), any());
     }
 
     @Test
-    @DisplayName("MemberController - 검색 기록 조회")
     public void getSearchLog() throws Exception {
         // given
         Long loginMemberId = 1L;
@@ -161,25 +166,23 @@ public class MemberControllerTest {
 
         // when, then
         mockMvc.perform(get("/api/members/search-log")
-                        .param("id", loginMemberId.toString())
-                        .header("Authorization", "Bearer access-token"))
+                        .param("id", loginMemberId.toString()))
                 .andExpect(status().isOk());
-        verify(memberService).getSearchLog(anyLong());
+        verify(memberService).getSearchLog(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 회원 팀 조회")
     public void getMemberTeams() throws Exception {
+        // given
         Long id = 1L;
-        mockMvc.perform(get("/api/members/teams")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer access-token"))
+
+        // when, then
+        mockMvc.perform(get("/api/members/teams"))
                 .andExpect(status().isOk());
         verify(memberService).getMemberTeams(any());
     }
 
     @Test
-    @DisplayName("Member Controller - 멤버 성취율 조회")
     public void getMemberAchievement() throws Exception {
         // given
         Long id = 1L;
