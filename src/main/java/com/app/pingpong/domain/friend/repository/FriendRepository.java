cc9aa82459525831f21111d475d849f9507c15d0
yaezzin <yaezzin@gmail.com>
2024-04-08 16:32:46 +0900

    fix : @Param 명시하기

--- src/main/java/com/app/pingpong/domain/friend/repository/FriendRepository.java
+++ src/main/java/com/app/pingpong/domain/friend/repository/FriendRepository.java
@@ -4,6 +4,7 @@ import com.app.pingpong.domain.friend.entity.Friend;
 import com.app.pingpong.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,11 +14,11 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     @Query("SELECT m FROM Member m " +
             "JOIN Friend f ON f.applicant = m.id " +
             "WHERE f.respondent = :respondentId AND f.status = 'ACTIVE'")
-    List<Member> findAllFriendsByRespondent(Long respondentId);
+    List<Member> findAllFriendsByRespondent(@Param("respondentId") Long respondentId);
 
     @Query("SELECT m FROM Member m " +
             "JOIN Friend f ON f.respondent = m.id " +
             "WHERE f.applicant = :applicantId AND f.status = 'ACTIVE'")
-    List<Member> findAllFriendsByApplicant(Long applicantId);
+    List<Member> findAllFriendsByApplicant(@Param("applicantId") Long applicantId);
 }
 
